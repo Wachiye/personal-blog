ee87270df30c100d03376f22e74da97c6c318098
Wachiye <47672963+Wachiye@users.noreply.github.com>
2020-01-13 19:25:03 +0300

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -2,6 +2,11 @@
 <html>
 <head>
 	<title>Sirah</title>
+<meta charset="UTF-8">
+  <meta name="description" content="Personal Blog">
+  <meta name="keywords" content="wachiye, siranjofu, Jeremiah, sirah">
+  <meta name="author" content="Wachiye Jeremiah Siranjofu">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="static/css/style.css">
 </head>
 <body>
