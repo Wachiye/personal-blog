83b992871b1ea7f67f1150669a4d50f65093e765
Wachiye <47672963+Wachiye@users.noreply.github.com>
2020-01-14 11:51:47 +0300

    updated header.php, included icon

--- includes/header.php
+++ includes/header.php
@@ -2,12 +2,13 @@
 <html>
 <head>
 	<title>Sirah</title>
-<meta charset="UTF-8">
-  <meta name="description" content="Personal Blog">
-  <meta name="keywords" content="wachiye, siranjofu, Jeremiah, sirah">
-  <meta name="author" content="Wachiye Jeremiah Siranjofu">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<meta name="description" content="Personal Blog">
+	<meta name="keywords" content="wachiye, siranjofu, Jeremiah, sirah">
+	<meta name="author" content="Wachiye Jeremiah Siranjofu">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="static/css/style.css">
+	<link rel="icon" href=""static/img/icon.ico">
 </head>
 <body>
 <div class="mainPanel">
