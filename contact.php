7c70f38fe29a9e6154a22055490eff2c7f84a928
Wachiye <47672963+Wachiye@users.noreply.github.com>
2020-05-30 12:16:34 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -11,6 +11,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> 
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        
 	<link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
@@ -60,7 +61,8 @@
 			<div class="col-sm-7 my-1">
 				<p class="text-info">Have an issue? Need some help or clarification? Let's get in touch. All fields are required.
 				</p>
-				<form action="https://docs.google.com/forms/d/e/1FAIpQLSdVi0QCae9ZvoMgIc_CEIR6CzZ8SLQnWL0KhIuTToo_NmXoyQ/formResponse" method="post" >
+				<form action="https://docs.google.com/forms/d/e/1FAIpQLSdVi0QCae9ZvoMgIc_CEIR6CzZ8SLQnWL0KhIuTToo_NmXoyQ/formResponse" method="post" onsubmit="window.alert('Message sent successfully. Thank you.')">
+                                        
 					<div class="form-group">
 						<label for="entry.2005620554">Name</label>
 						<input type="text" class="form-control form-control-sm" name="entry.2005620554" placeholder="Enter your name" required>
