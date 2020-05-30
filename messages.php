f7ed6c51640f07e44af827d5ce2d8636e66a7e36
Wachiye <47672963+Wachiye@users.noreply.github.com>
2020-05-30 12:49:15 +0300

    Update messages.php

--- messages.php
+++ messages.php
@@ -1,31 +1,63 @@
-<?php
-
-if (!empty($_POST['submit'])) {
-
-	$type = "";
-
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$phone = $_POST['phone'];
-	$subject = "New Portfolio Contact Form Received";
-
-	$emailOpener ="<p>Hey, you have a new message from your contact page.</p> \r\n ";
-	$emailOpener .= "<p>User Details: <br> Name: " .$name ." <br> Email : " .$email ." <br> Phone : " .$phone ."</p> \r\n";
-	$emailContent = $emailOpener ." <p> Message: <br>" .$_POST['message'];
-
-	$emailTo = "siranjofuw@gmail.com";
-
-	$emailHeaders = "From " .$name ."<" .$email ."> \r\n";
-
-	if(mail($emailTo, $subject, $emailContent, $emailHeaders)){
-		
-		$type = "success";
-	}
-	else{
-		$type = "error";
-	}
-
-	header("Location : ../contact.php?res =" .$type);
-}
-
-?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta name="description" content="#">
+	<meta name="author" content="Wachiye Siranjofu <siranjofuw@gmail.com>">
+	<meta name="keywords" content="wachiye, siranjofu, sirah, portfolio, website, blog, web, designer, developer">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Wachiye Siranjofu</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> 
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> 
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row " id="header">
+			<div class="col-12 d-sm-none" id="sm-header">
+				<div>I'm Sirah</div>
+			</div>
+			<div class="col-sm-4 d-none d-sm-block" id="header-image">
+				<img src="./img/me.jpeg" class="img-circle" width="50px" height="50px" alt="me" >
+			</div>
+			<div class="col-sm-4 d-none d-sm-block" id="header-name">
+				<h4>Wachiye Siranjofu</h4>
+				<small class="small lead">Web Designer/ Developer.</small>
+			</div>
+			<div class="col-sm-4 d-none d-sm-block" id="header-contacts">
+				<a href="tel:254790983123">
+					<i class="fa fa-phone"></i> +254 - 7 - 9098 - 3123
+				</a>
+				<a href="mailto:siranjofuw@gmail.com">
+					<i class="fa fa-envelope"></i> siranjofuw@gmail.com
+				</a>
+				<div>
+					<i class="fa fa-map"></i> Kiminini, Kitale, Kenya
+				</div>
+			</div>
+		</div>
+		<div class="row" id="navigation">
+			<div class="col-12" >
+				<ul class="nav">
+					<li class="nav-item">
+						<a class="nav-link" href="./index.html">Home</a>
+					</li>
+					<li class="nav-item ">
+						<a class="nav-link" href="./resume.html">CV</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="./samples.html">Activities</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" href="./contact.html">Contact</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+                <div class="row">
+                        <div class="col-12">
+                          <p class="text-success">Your message has been received and will be acted upon ASAP. Thank you
+                        </div>
+                </div>
