f1b46190bdcb15359e68876df401815e94a46ae0
Wachiye <siranjofuw@gmail.com>
2023-03-05 18:30:01 +0300

    updated .gitigore

--- build/middleware/not-found.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notFoundHandler = void 0;
-const notFoundHandler = (request, response, next) => {
-    const message = "Resource not found";
-    response.status(404).json({ message: message });
-};
-exports.notFoundHandler = notFoundHandler;
-//# sourceMappingURL=not-found.middleware.js.map
